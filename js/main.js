d7d24b8356efac5f34be628edef1aed1d0154635
toth8j <toth.jozsef8@mav-szk.hu>
2017-05-26 16:47:51 +0200

    HTML5_2 feltöltése 3. nap

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Módosítjuk a h1 elem tartalmát.
+document.querySelector("h1").innerHTML = "Hello ITFactory";
+
+// Kép forrásának módosítása.
+function changeUrl() {
+    document.querySelector("img").src = "http://cms.sulinet.hu/get/d/c2ffefdd-3248-4bbc-b294-26c3159a112f/1/8/b/Normal/holdkorong.jpg";
+    
+}
+
+// Stílus módosítása.
+function changeStyle() {
+    document.querySelector("img").style.width = "80px";
+    
+}
